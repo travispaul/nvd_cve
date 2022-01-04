928181ec67b814dac4b6a796e22da827df8d2769
Travis Paul <tr@vispaul.me>
2022-01-04 08:55:02 -0500

    Remove uneccesary SQLiteCache struct

--- src/cache.rs
+++ src/cache.rs
@@ -9,6 +9,7 @@ use std::fmt;
 use std::path::PathBuf;
 use std::{env, fs, io};
 
+/// Configuration details about how to sync remote feeds to a local SQLite database.
 #[derive(Debug)]
 pub struct CacheConfig {
     pub url: String,
@@ -19,17 +20,18 @@ pub struct CacheConfig {
 }
 
 impl CacheConfig {
-    /// If a full path wasn't supplied for the local database, then try to pick something reasonable based on:
-    /// https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+    /// If a full path wasn't supplied for the local database, then try to pick something reasonable
+    /// based on the
+    /// [XDG Base Directory Spec](https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html).
     ///
-    /// $XDG_CACHE_HOME defines the base directory relative to which user specific
-    /// non-essential data files should be stored.
+    /// > _`$XDG_CACHE_HOME` defines the base directory relative to which user specific
+    /// > non-essential data files should be stored._
     ///
-    /// If $XDG_CACHE_HOME is either not set or empty, a default equal to
-    /// $HOME/.cache should be used.
+    /// > _If `$XDG_CACHE_HOME` is either not set or empty, a default equal to
+    /// > `$HOME/.cache` should be used._
     ///
-    /// If $HOME can't be determined, stray from the basedir spec and try the OS's temporary directory
-    /// Failing that, set a relative path.
+    /// If `$HOME` can't be determined, stray from the basedir spec and try the OS's temporary
+    /// directory. Failing that, set a relative path.
     pub fn default_db_path() -> String {
         let mut path = std::path::PathBuf::new();
         let cache_namespace = "nvd";
@@ -61,6 +63,7 @@ impl CacheConfig {
         fallback.to_str().unwrap().to_string()
     }
 
+    /// Create a new CacheConfig with some sane defaults.
     pub fn new() -> Self {
         let mut feeds: Vec<String> = (2002..2022).into_iter().map(|x| x.to_string()).collect();
         feeds.push("recent".to_string());
@@ -133,98 +136,91 @@ impl From<serde_json::Error> for CacheError {
     }
 }
 
-pub struct SqliteCache {
-    path: String,
-}
+fn create_schema(path: &str) -> Result<(), CacheError> {
+    let mut db_path = PathBuf::from(&path);
+    db_path.pop();
+    fs::create_dir_all(db_path)?;
 
-impl SqliteCache {
-    pub fn new<S: Into<String>>(path: S) -> Self {
-        Self { path: path.into() }
-    }
+    let conn = Connection::open(&path)?;
 
-    pub fn create_schema(&self) -> Result<(), CacheError> {
-        let mut db_path = PathBuf::from(&self.path);
-        db_path.pop();
-        fs::create_dir_all(db_path)?;
+    let mut tbl_stmt =
+        conn.prepare("SELECT name FROM sqlite_master where type = 'table' and name = ?;")?;
 
-        let conn = Connection::open(&self.path)?;
-
-        let mut tbl_stmt =
-            conn.prepare("SELECT name FROM sqlite_master where type = 'table' and name = ?;")?;
-
-        if !tbl_stmt.exists(["cve"])? {
-            conn.execute(
-                "CREATE TABLE cve (
+    if !tbl_stmt.exists(["cve"])? {
+        conn.execute(
+            "CREATE TABLE cve (
                id VARCHAR PRIMARY KEY,
                description TEXT,
                data TEXT NOT NULL)",
-                [],
-            )?;
-        }
+            [],
+        )?;
+    }
 
-        if !tbl_stmt.exists(["metafile"])? {
-            conn.execute(
-                "CREATE TABLE metafile (
+    if !tbl_stmt.exists(["metafile"])? {
+        conn.execute(
+            "CREATE TABLE metafile (
                 feed VARCHAR PRIMARY KEY,
                 last_modified_date VARCHAR NOT NULL,
                 size INTEGER NOT NULL,
                 zip_size INTEGER NOT NULL,
                 gz_size INTEGER NOT NULL,
                 sha256 VARCHAR NOT NULL)",
-                [],
-            )?;
-        }
-
-        tbl_stmt.finalize()?;
-
-        match conn.close() {
-            Ok(_) => Ok(()),
-            Err((_, error)) => Err(CacheError::RusqliteError(error)),
-        }
+            [],
+        )?;
     }
 
-    pub fn get_metafiles(&self, feeds: &[String]) -> Result<Vec<Feed>, CacheError> {
-        let conn = Connection::open(&self.path)?;
+    tbl_stmt.finalize()?;
 
-        let mut stmt = conn.prepare("SELECT * FROM metafile where feed=?1")?;
-
-        let cached_feeds = feeds
-            .iter()
-            .map(|name| {
-                let meta = stmt
-                    .query_row([&name], |row| {
-                        let last_modified_row: String =
-                            row.get("last_modified_date").unwrap_or_default();
-                        let last_modified_date =
-                            Metafile::parse_datetime(last_modified_row.as_str());
-                        let metafile = Metafile {
-                            last_modified_date,
-                            size: row.get("size").unwrap_or_default(),
-                            zip_size: row.get("zip_size").unwrap_or_default(),
-                            gz_size: row.get("gz_size").unwrap_or_default(),
-                            sha256: row.get("sha256").unwrap_or_default(),
-                        };
-                        Ok(metafile)
-                    })
-                    .ok();
-                Feed {
-                    name: name.clone(),
-                    metafile: meta,
-                }
-            })
-            .collect();
-
-        stmt.finalize()?;
-
-        match conn.close() {
-            Ok(_) => Ok(cached_feeds),
-            Err((_, error)) => Err(CacheError::RusqliteError(error)),
-        }
+    match conn.close() {
+        Ok(_) => Ok(()),
+        Err((_, error)) => Err(CacheError::RusqliteError(error)),
     }
+}
 
-    pub fn set_metafile(&self, feed: &str, metafile: &Metafile) -> Result<(), CacheError> {
-        let conn = Connection::open(&self.path)?;
-        let upsert_sql = "
+/// Get all cached Metafiles
+fn get_metafiles(config: &CacheConfig) -> Result<Vec<Feed>, CacheError> {
+    let conn = Connection::open(&config.db)?;
+
+    let mut stmt = conn.prepare("SELECT * FROM metafile where feed=?1")?;
+
+    let cached_feeds = config.feeds
+        .iter()
+        .map(|name| {
+            let meta = stmt
+                .query_row([&name], |row| {
+                    let last_modified_row: String =
+                        row.get("last_modified_date").unwrap_or_default();
+                    let last_modified_date =
+                        Metafile::parse_datetime(last_modified_row.as_str());
+                    let metafile = Metafile {
+                        last_modified_date,
+                        size: row.get("size").unwrap_or_default(),
+                        zip_size: row.get("zip_size").unwrap_or_default(),
+                        gz_size: row.get("gz_size").unwrap_or_default(),
+                        sha256: row.get("sha256").unwrap_or_default(),
+                    };
+                    Ok(metafile)
+                })
+                .ok();
+            Feed {
+                name: name.clone(),
+                metafile: meta,
+            }
+        })
+        .collect();
+
+    stmt.finalize()?;
+
+    match conn.close() {
+        Ok(_) => Ok(cached_feeds),
+        Err((_, error)) => Err(CacheError::RusqliteError(error)),
+    }
+}
+
+/// Update or insert Metafile
+fn update_metafile(config: &CacheConfig, feed: &str, metafile: &Metafile) -> Result<(), CacheError> {
+    let conn = Connection::open(&config.db)?;
+    let upsert_sql = "
         insert into
         metafile (
             feed,
@@ -244,8 +240,8 @@ impl SqliteCache {
             gz_size = ?5,
             sha256 = ?6;";
 
-        let mut stmt = conn.prepare(upsert_sql)?;
-        stmt.insert(params![
+    let mut stmt = conn.prepare(upsert_sql)?;
+    stmt.insert(params![
             feed,
             metafile.format_last_modified_date(),
             metafile.size,
@@ -253,20 +249,21 @@ impl SqliteCache {
             metafile.gz_size,
             metafile.sha256
         ])?;
-        stmt.finalize()?;
-        match conn.close() {
-            Ok(_) => Ok(()),
-            Err((_, error)) => Err(CacheError::RusqliteError(error)),
-        }
+    stmt.finalize()?;
+    match conn.close() {
+        Ok(_) => Ok(()),
+        Err((_, error)) => Err(CacheError::RusqliteError(error)),
     }
+}
 
-    pub fn set_cves(
-        &self,
-        cve_feed: &[CveContainer],
-        last_modified_date: Option<&NaiveDateTime>,
-    ) -> Result<(), CacheError> {
-        let conn = Connection::open(&self.path)?;
-        let upsert_sql = "
+// Update or insert CVEs from a CVEContainer
+fn update_cves(
+    config: &CacheConfig,
+    cve_feed: &[CveContainer],
+    last_modified_date: Option<&NaiveDateTime>,
+) -> Result<(), CacheError> {
+    let conn = Connection::open(&config.db)?;
+    let upsert_sql = "
         insert into
         cve (
             id,
@@ -280,100 +277,53 @@ impl SqliteCache {
             description=?2,
             data=?3;";
 
-        let mut stmt = conn.prepare(upsert_sql)?;
-        let mut unecessary = 0;
+    let mut stmt = conn.prepare(upsert_sql)?;
+    let mut unecessary = 0;
 
-        // We can't borrow conn immutably for the prepared statement AND mutably for a transaction
-        // Transaction::new_unchecked() allows for an immutable borrow of the connection
-        // see: https://github.com/rusqlite/rusqlite/pull/693
-        let tx = Transaction::new_unchecked(&conn, TransactionBehavior::Exclusive)?;
+    // We can't borrow conn immutably for the prepared statement AND mutably for a transaction
+    // Transaction::new_unchecked() allows for an immutable borrow of the connection
+    // see: https://github.com/rusqlite/rusqlite/pull/693
+    let tx = Transaction::new_unchecked(&conn, TransactionBehavior::Exclusive)?;
 
-        for cve in cve_feed {
-            let mut skip = false;
+    for cve in cve_feed {
+        let mut skip = false;
 
-            if let Some(metafile_datetime) = last_modified_date {
-                if let Ok(cve_datetime) =
-                    NaiveDateTime::parse_from_str(&cve.last_modified_date, "%Y-%m-%dT%H:%M%Z")
-                {
-                    if cve_datetime > *metafile_datetime {
-                        skip = true;
+        if let Some(metafile_datetime) = last_modified_date {
+            if let Ok(cve_datetime) =
+            NaiveDateTime::parse_from_str(&cve.last_modified_date, "%Y-%m-%dT%H:%M%Z")
+            {
+                if cve_datetime > *metafile_datetime {
+                    skip = true;
+                }
+            }
+        }
+
+        if skip {
+            unecessary += 1;
+        } else {
+            let mut description = None;
+            if !cve.cve.description.description_data.is_empty() {
+                for d in &cve.cve.description.description_data {
+                    if d.lang == "en" {
+                        description = Some(String::from(&d.value));
                     }
                 }
             }
-
-            if skip {
-                unecessary += 1;
-            } else {
-                let mut description = None;
-                if !cve.cve.description.description_data.is_empty() {
-                    for d in &cve.cve.description.description_data {
-                        if d.lang == "en" {
-                            description = Some(String::from(&d.value));
-                        }
-                    }
-                }
-                stmt.insert(params![
+            stmt.insert(params![
                     cve.cve.cve_data_meta.id,
                     description,
                     serde_json::to_string(&cve.cve).unwrap_or_else(|_| { "{}".to_string() })
                 ])?;
-            }
-        }
-
-        tx.commit()?;
-
-        debug!("Skipped {} unnecessary inserts", unecessary);
-        stmt.finalize()?;
-        match conn.close() {
-            Ok(_) => Ok(()),
-            Err((_, error)) => Err(CacheError::RusqliteError(error)),
         }
     }
 
-    pub fn get_cve(&self, cve: &str) -> Result<Cve, CacheError> {
-        let conn = Connection::open(&self.path)?;
+    tx.commit()?;
 
-        let mut stmt = conn.prepare("SELECT * FROM cve where id=?1")?;
-
-        let data = stmt.query_row([&cve], |row| {
-            let data: String = row.get("data")?;
-            Ok(data)
-        })?;
-
-        stmt.finalize()?;
-
-        let result: Cve = serde_json::from_str(data.as_str())?;
-
-        match conn.close() {
-            Ok(_) => Ok(result),
-            Err((_, error)) => Err(CacheError::RusqliteError(error)),
-        }
-    }
-
-    pub fn search_cve_description(&self, text: &str) -> Result<usize, CacheError> {
-        let conn = Connection::open(&self.path)?;
-
-        let mut count = 0;
-
-        let mut stmt =
-            conn.prepare("SELECT id FROM cve where description like '%' || ?1 || '%'")?;
-
-        let cves = stmt.query_map(params![text], |row| {
-            let id: String = row.get("id")?;
-            Ok(id)
-        })?;
-
-        for cve in cves {
-            count += 1;
-            println!("{}", cve?.as_str());
-        }
-
-        stmt.finalize()?;
-
-        match conn.close() {
-            Ok(_) => Ok(count),
-            Err((_, error)) => Err(CacheError::RusqliteError(error)),
-        }
+    debug!("Skipped {} unnecessary inserts", unecessary);
+    stmt.finalize()?;
+    match conn.close() {
+        Ok(_) => Ok(()),
+        Err((_, error)) => Err(CacheError::RusqliteError(error)),
     }
 }
 
@@ -382,7 +332,6 @@ pub fn sync_blocking<C: BlockingHttpClient>(
     client: C,
 ) -> Result<(), CacheError> {
     let mut bar = progress::Bar::new();
-    let cache = SqliteCache::new(&config.db);
 
     let mut synced = 0;
 
@@ -394,9 +343,9 @@ pub fn sync_blocking<C: BlockingHttpClient>(
         bar.reach_percent((synced as f32 / to_sync as f32 * 100.0).round() as i32);
     }
 
-    cache.create_schema()?;
+    create_schema(&config.db)?;
 
-    let feeds = cache.get_metafiles(&config.feeds)?;
+    let feeds = get_metafiles(config)?;
 
     for feed in feeds {
         let mut last_modified = None;
@@ -452,14 +401,14 @@ pub fn sync_blocking<C: BlockingHttpClient>(
             bar.reach_percent((synced as f32 / to_sync as f32 * 100.0).round() as i32);
         }
 
-        cache.set_cves(&cve_feed.cve_items, last_modified)?;
+        update_cves(config, &cve_feed.cve_items, last_modified)?;
 
         if config.show_progress {
             synced += 1;
             bar.reach_percent((synced as f32 / to_sync as f32 * 100.0).round() as i32);
         }
 
-        cache.set_metafile(&feed.name, &metafile)?;
+        update_metafile(config, &feed.name, &metafile)?;
 
         if config.show_progress {
             synced += 1;
@@ -471,11 +420,47 @@ pub fn sync_blocking<C: BlockingHttpClient>(
 }
 
 pub fn search_by_id(config: &CacheConfig, cve: &str) -> Result<Cve, CacheError> {
-    let cache = SqliteCache::new(&config.db);
-    cache.get_cve(cve)
+    let conn = Connection::open(&config.db)?;
+
+    let mut stmt = conn.prepare("SELECT * FROM cve where id=?1")?;
+
+    let data = stmt.query_row([&cve], |row| {
+        let data: String = row.get("data")?;
+        Ok(data)
+    })?;
+
+    stmt.finalize()?;
+
+    let result: Cve = serde_json::from_str(data.as_str())?;
+
+    match conn.close() {
+        Ok(_) => Ok(result),
+        Err((_, error)) => Err(CacheError::RusqliteError(error)),
+    }
 }
 
 pub fn search_description(config: &CacheConfig, text: &str) -> Result<usize, CacheError> {
-    let cache = SqliteCache::new(&config.db);
-    cache.search_cve_description(text)
+    let conn = Connection::open(&config.db)?;
+
+    let mut count = 0;
+
+    let mut stmt =
+        conn.prepare("SELECT id FROM cve where description like '%' || ?1 || '%'")?;
+
+    let cves = stmt.query_map(params![text], |row| {
+        let id: String = row.get("id")?;
+        Ok(id)
+    })?;
+
+    for cve in cves {
+        count += 1;
+        println!("{}", cve?.as_str());
+    }
+
+    stmt.finalize()?;
+
+    match conn.close() {
+        Ok(_) => Ok(count),
+        Err((_, error)) => Err(CacheError::RusqliteError(error)),
+    }
 }
